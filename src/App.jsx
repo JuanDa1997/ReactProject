549629ab3bee8f1fc2de93d4ef5a3cc656039395
Juan David Ruiz Lopez <49179416+JuanDa1997@users.noreply.github.com>
2021-05-25 12:04:27 -0500

    First commit

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import './App.css';
+// import './App.scss';
+
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+       
+       <h1>hola fucking mundo</h1>
+      </header>
+    </div>
+  );
+}
+
+export default App;

9cc9236cb6f4f4b5cf80488149e7797b7d8c6e1a
Juan David Ruiz Lopez <49179416+JuanDa1997@users.noreply.github.com>
2021-05-25 21:20:03 -0500

    Header v2

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,5 +1,9 @@
 import React, { Component } from 'react';
 import logo from '../../asset/images/logo.png';
+import menu from '../../asset/images/menu.png';
+import exit from '../../asset/images/cancel.png';
+
+
 
 
 class Header extends Component {
@@ -7,14 +11,33 @@ class Header extends Component {
 
         return(
            
-            <div className="Header">
-                <img src={logo} className="logo-game" alt="logo" />
-                <nav>
-                   <a href="">Home</a>
-                   <a href="">About Us</a>
-                   <a href="">Contacts</a>
+            <header className="Header">
+                <nav className="menu-principal">
+                    <button className="open-menu" aria-label="abrir menú">
+                        <img src={menu} alt="abrir menú" />
+                    </button>
+                    <ul className="Menu">
+                        <li><a href="">Home</a></li>
+                        <li><a href="">About Us</a></li>
+                        <li><a href="">Contacts</a></li>
+                    </ul>
                 </nav>
-            </div>
+                <nav className="menu-secundario">
+                    <ul className = "Menu-Hamburguesa">
+                        <button className="close-menu" aria-label="abrir menú">
+                            <img src={exit} alt="cerrar menú" />
+                        </button>
+                        <li><a href="">Home</a></li>
+                        <li><a href="">About Us</a></li>
+                        <li><a href="">Contacts</a></li>
+                    </ul>
+                </nav>
+                
+                <div class="container-logo">
+                    <img src={logo} className="logo-game" alt="logo" />
+                </div>
+
+            </header>
         );
     }
 }
